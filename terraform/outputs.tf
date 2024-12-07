3dd81ff7dc95e3c8d8dfa333cf801af74bf85013
NewtonChutney <70827815+NewtonChutney@users.noreply.github.com>
2024-12-07 19:35:24 +0530

    infra deployment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,24 +2,24 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-# output "azurerm_mysql_flexible_server" {
-#   value = azurerm_mysql_flexible_server.default.name
-# }
+output "azurerm_mysql_flexible_server" {
+  value = azurerm_mysql_flexible_server.default.name
+}
 
-# output "admin_login" {
-#   value = azurerm_mysql_flexible_server.default.administrator_login
-# }
+output "admin_login" {
+  value = azurerm_mysql_flexible_server.default.administrator_login
+}
 
-# output "admin_password" {
-#   sensitive = true
-#   value     = azurerm_mysql_flexible_server.default.administrator_password
-# }
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_mysql_flexible_server.default.administrator_password
+}
 
-# output "mysql_flexible_server_database_name" {
-#   value = azurerm_mysql_flexible_database.default.name
-# }
+output "mysql_flexible_server_database_name" {
+  value = azurerm_mysql_flexible_database.default.name
+}
 
-# output "kube_config" {
-#   value = azurerm_kubernetes_cluster.aks.kube_config_raw
-#   sensitive = true
-# }
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
